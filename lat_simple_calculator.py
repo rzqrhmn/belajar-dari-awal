1c1367ee6f99508fb2466e8683e0e8276d2ffe13
rzqrhmn <rizqia.rahman@stud.hs-kempten.de>
2025-08-28 19:50:22 +0200

    Updated, Simple Calculator

--- lat_simple_calculator.py
+++ lat_simple_calculator.py
@@ -1,7 +1,20 @@
+def tambah(a, b): return a + b
+def kurang(a, b): return a - b
+def kali(a, b): return a * b
+def bagi(a, b): return a / b
+
+print("===== Calculator =====")
+print("1. Sum\n2. Substract\n3. Multiply\n4. Divide")
+
+option = int(input("Choose the operation (1-4): "))
 a = int(input("Input 1st number = "))
 b = int(input("Input 2nd number = "))
-print("so,", a,"+",b," = ", a+b)
-print(a,"-",b," = ", a-b)
-print(a,"x",b," = ", a*b)
-print(a,"/",b," = ", a/b)
-print(a,"^",b," = ", a**b)
+
+if option == 1:
+    print("Result: ", tambah(a,b))
+elif option == 2:
+    print("Result: ", kurang(a,b))
+elif option == 3:
+    print("Result: ", kali(a,b))
+elif option == 4:
+    print("Result: ", bagi(a,b))
